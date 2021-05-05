35e4038e636a44047711da6cee4fc388313b66d0
DaniilKim <liinad.mik@gmail.com>
2021-05-05 12:42:09 +0300

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Daniil Kim on 05.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
